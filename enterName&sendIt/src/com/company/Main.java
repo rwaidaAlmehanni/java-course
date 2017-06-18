8f109be39b4535d9bf0a19d5f1fea5fda481c715
rwaida <rwaidaAlmehanni>
2017-06-18 14:01:08 +0300

    enter name and send it to new class

---
+++ enterName&sendIt/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+           display x=new display();
+           Scanner input= new Scanner(System.in);
+           System.out.println("enter your name :");
+           x.displaymess(input.nextLine());
+
+    }
+}
---
+++ enterName&sendIt/src/com/company/display.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by Rwaida on 6/18/2017.
+ */
+public class display {
+    public void displaymess(String name){
+        System.out.println("hello "+name);
+    }
+}
