8cae2eb8da6fb66a90e281b57da96323b23c0a43
rwaida <rwaidaAlmehanni>
2017-06-18 16:04:41 +0300

    creating array

---
+++ creatingArray/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+        int x[]=new int[3];
+        String y[]={"a","b","c"};
+        x[0]=99;
+        x[2]=3;
+        System.out.println(x[2]);
+        for(int i=0;i<y.length;i++){
+            System.out.println(y[i]);
+        }
+
+    }
+}
