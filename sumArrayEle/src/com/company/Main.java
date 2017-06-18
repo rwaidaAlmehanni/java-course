7c71a2b9ad5b50c0a8726fa52fea720c59515f73
rwaida <rwaidaAlmehanni>
2017-06-18 16:19:23 +0300

    sum element of the array

---
+++ sumArrayEle/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+       int y[]={3,5,22};
+       int sum=0;
+       for (int i=0;i<y.length;i++){
+           sum+=y[i];
+       }
+       System.out.println(sum);
+
+        }
+}
