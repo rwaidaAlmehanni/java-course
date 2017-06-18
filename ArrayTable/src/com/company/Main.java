828fdeac0c4569839b3ccbd39a19b859ac2d036d
rwaida <rwaidaAlmehanni>
2017-06-18 16:13:36 +0300

    create an array table

---
+++ ArrayTable/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        String y[]={"a","b","c"};
+        System.out.println("index\tvalue");
+        for (int i=0;i<y.length;i++){
+            System.out.println(i +"\t    "+y[i]);
+        }
+
+        }
+
+}
