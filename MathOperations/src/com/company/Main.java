5b1b79d9e2dba4e85b739415961d310ccf354c56
rwaida <rwaidaAlmehanni>
2017-06-18 15:40:24 +0300

    the math operta

---
+++ MathOperations/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+         System.out.println("5,1 the Maximum value : "+Math.max(5,1));
+         System.out.println("5,1 the Minimum value : "+Math.min(5,1));
+         System.out.println("5,2 the Power value : "+Math.pow(5,2));
+         System.out.println("64 the Sqrt value : "+Math.sqrt(64));
+         System.out.println("-44.3 the absolute value : "+Math.abs(-44.3));
+
+    }
+}
