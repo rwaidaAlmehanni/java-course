a712cb18bb610941f6021dfe12bbb17659ac9eb9
rwaida <rwaidaAlmehanni>
2017-06-18 15:29:47 +0300

    do while example

---
+++ doWhile/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+     int counter=0;
+     do{
+         System.out.println(counter);
+         counter++;
+     }while(counter<=10);
+    }
+}
