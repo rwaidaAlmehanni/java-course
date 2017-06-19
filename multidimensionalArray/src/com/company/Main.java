189e5c6995efef228cd0e1bcce26f2b0565b2050
rwaida <rwaidaAlmehanni>
2017-06-19 11:21:37 +0300

    easiest way to create multidimensional (Matrix) array

---
+++ multidimensionalArray/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        int x[][]={{1,2,3},{4,5,6}};
+        int y[][]={{1,2,3},{4},{5,3,4,2}};
+        
+    }
+}
