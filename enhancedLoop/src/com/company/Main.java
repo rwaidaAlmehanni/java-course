136510dc7c059854943df0e8856cc3b201f5b773
rwaida <rwaidaAlmehanni>
2017-06-19 10:57:21 +0300

    enhanced for loop

---
+++ enhancedLoop/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+       int y[]={3,5,22};
+       int sum=0;
+       for (int i:y){
+           sum+=i;
+          }
+       System.out.println(sum);
+
+        }
+}
