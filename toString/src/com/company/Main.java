6523006bf2fa2d90868336da81038d904ab5aad5
rwaida <rwaidaAlmehanni>
2017-06-20 10:41:02 +0300

    generate date formate

---
+++ toString/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        time x1=new time(2,6,9);
+
+        System.out.printf("%s\n",x1.toString());
+
+    }
+
+}
