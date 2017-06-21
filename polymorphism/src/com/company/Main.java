f1366c8bdb6f6a5f2dc6232539eeb8dfa9335130
rwaida <rwaidaAlmehanni>
2017-06-21 15:20:31 +0300

    introduction in polymorphism

---
+++ polymorphism/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.EnumSet;
+import javax.swing.JFrame;
+import static java.lang.String.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+           father x[]=new father[2];
+           x[0]=new chaild1();
+           x[1]=new chaild2();
+           for (father i:x){
+               i.eat();
+        }
+
+       }
+
+}
---
+++ polymorphism/src/com/company/chaild1.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by Rwaida on 6/21/2017.
+ */
+public class chaild1 extends father {
+     void eat() {
+        System.out.println("i am the chaild1 class :(((");
+    }
+}
