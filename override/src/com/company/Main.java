8b3e2be91e07daa3b8ed6385ed39a40d0ac67d04
rwaida <rwaidaAlmehanni>
2017-06-21 10:40:43 +0300

    small ex about override

---
+++ override/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.EnumSet;
+
+import static java.lang.String.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+          father y=new father();
+          child x=new child();
+          x.output();
+          y.output();
+
+       }
+
+}
---
+++ override/src/com/company/child.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by Rwaida on 6/21/2017.
+ */
+public class child extends father {
+    public void output(){
+        System.out.println("hello i am the overwride in child class :)");
+    }
+}
